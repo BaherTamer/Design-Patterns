aad019e735b544caa62fa5d6c908ee31fd30713c
Baher Tamer <baher.appledev@gmail.com>
2026-01-29 11:44:04 +0200

    chore: create xcode project

---
+++ Behavioral/Template Method/TemplateMethodExample/TemplateMethodExample/TemplateMethodExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateMethodExampleApp.swift
+//  TemplateMethodExample
+//
+//  Created by Baher Tamer on 29/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TemplateMethodExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
